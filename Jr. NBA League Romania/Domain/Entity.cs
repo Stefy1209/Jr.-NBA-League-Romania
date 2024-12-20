c091444c86dc8c2cae61a48d1421ec5db202d217
Stefy1209 <stefangramada09@gmail.com>
2024-12-20 15:48:21 +0200

    Start of the project

---
+++ Jr. NBA League Romania/Domain/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace Jr._NBA_League_Romania.Domain;
+
+public class Entity<TId>(TId id)
+{
+    public TId Id { get; } = id;
+}
---
+++ Jr. NBA League Romania/Domain/Team.cs
@@ -0,0 +1,6 @@
+﻿namespace Jr._NBA_League_Romania.Domain;
+
+public class Team(Guid id, string name) : Entity<Guid>(id)
+{
+    public string Name { get; } = name;
+}
