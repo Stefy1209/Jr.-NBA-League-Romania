eeb0cd5cdcb45cea125022a479f7231dba001770
Stefy1209 <stefangramada09@gmail.com>
2024-12-23 18:16:40 +0200

    Entity Test Added

---
+++ Jr. NBA League Romania.Tests/DomainTests/EntityTests.cs
@@ -0,0 +1,19 @@
+﻿using Jr._NBA_League_Romania.Domain;
+
+namespace Jr._NBA_League_Romania.Tests.DomainTests;
+
+public class EntityTests
+{
+    [Fact]
+    public void Entity_GetId_ReturnTid()
+    {
+        //Arrange
+        var entity = new Entity<int>(1);
+
+        //Act
+        var id = entity.Id;
+
+        //Assert
+        Assert.Equal(1, id);
+    }
+}
--- Jr. NBA League Romania.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Jr._NBA_League_Romania.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
