6a6af3953df0324905ea65bcc4de463873f105c9
Stefy1209 <stefangramada09@gmail.com>
2024-12-23 21:32:24 +0200

    Player.cs added

---
+++ Jr. NBA League Romania/Domain/Player.cs
@@ -0,0 +1,6 @@
+﻿namespace Jr._NBA_League_Romania.Domain;
+
+public class Player(Guid id, string name, string school, Guid teamId) : Student(id, name, school)
+{
+    public Guid TeamId { get; } = teamId;
+}
