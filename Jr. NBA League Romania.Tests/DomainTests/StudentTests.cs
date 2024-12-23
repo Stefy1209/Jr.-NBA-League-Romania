a87972a9c8065ef92bb6cf06590d8eef1f158177
Stefy1209 <stefangramada09@gmail.com>
2024-12-23 18:44:27 +0200

    StudentTests.cs GetSchool test added

--- Jr. NBA League Romania.Tests/DomainTests/StudentTests.cs
+++ Jr. NBA League Romania.Tests/DomainTests/StudentTests.cs
@@ -65,4 +65,35 @@ public class StudentTests
         Assert.Equal(name2, expectedName2);
         Assert.Equal(name3, expectedName3);
     }
+
+    [Fact]
+    public void Student_GetSchool_ReturnString()
+    {
+        //Arrange
+        var id1 = Guid.NewGuid();
+        var id2 = Guid.NewGuid();
+        var id3 = Guid.NewGuid();
+        
+        const string name1 = "Name1";
+        const string name2 = "Name2";
+        const string name3 = "Name3";
+        
+        const string school1 = "School1";
+        const string school2 = "School2";
+        const string school3 = "School3";
+        
+        var student1 = new Student(id1, name1, school1);
+        var student2 = new Student(id2, name2, school2);
+        var student3 = new Student(id3, name3, school3);
+        
+        //Act
+        var expectedSchool1 = student1.School;
+        var expectedSchool2 = student2.School;
+        var expectedSchool3 = student3.School;
+        
+        //Assert
+        Assert.Equal(school1, expectedSchool1);
+        Assert.Equal(school2, expectedSchool2);
+        Assert.Equal(school3, expectedSchool3);
+    }
 }
