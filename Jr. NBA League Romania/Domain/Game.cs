1aa9c77f0e024543101be56ddedbb24cdb09c123
Stefy1209 <stefangramada09@gmail.com>
2024-12-23 21:53:55 +0200

    Game.cs added

---
+++ Jr. NBA League Romania/Domain/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace Jr._NBA_League_Romania.Domain;
+
+public class Game(Guid id, Guid idTeam1, Guid idTeam2, DateTime dateTime) : Entity<Guid>(id)
+{
+    public Guid IdTeam1 { get; } = idTeam1;
+    public Guid IdTeam2 { get; } = idTeam2;
+    public DateTime DateTime { get; } = dateTime;
+}
