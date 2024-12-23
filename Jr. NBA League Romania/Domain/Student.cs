e1c27e4335c2b0c34f92c734d71c17981bd7f4d2
Stefy1209 <stefangramada09@gmail.com>
2024-12-23 18:50:15 +0200

    Student.cs added

---
+++ Jr. NBA League Romania/Domain/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace Jr._NBA_League_Romania.Domain;
+
+public class Student(Guid id, string name, string school) : Entity<Guid>(id)
+{
+    public string Name { get; } = name;
+    public string School { get; } = school;
+}
