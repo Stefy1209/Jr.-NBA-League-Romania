90720ad97977327e05bc95f7776a0bbe9b16c019
Stefy1209 <stefangramada09@gmail.com>
2024-12-23 18:49:59 +0200

    StudentTests.cs refactored

--- Jr. NBA League Romania.Tests/DomainTests/StudentTests.cs
+++ Jr. NBA League Romania.Tests/DomainTests/StudentTests.cs
@@ -4,96 +4,62 @@ namespace Jr._NBA_League_Romania.Tests.DomainTests;
 
 public class StudentTests
 {
+    //Arrange
+    private static readonly Guid Id1 = Guid.NewGuid();
+    private static readonly Guid Id2 = Guid.NewGuid();
+    private static readonly Guid Id3 = Guid.NewGuid();
+
+    private const string Name1 = "Name1";
+    private const string Name2 = "Name2";
+    private const string Name3 = "Name3";
+
+    private const string School1 = "School1";
+    private const string School2 = "School2";
+    private const string School3 = "School3";
+
+    private readonly Student _student1 = new(Id1, Name1, School1);
+    private readonly Student _student2 = new(Id2, Name2, School2);
+    private readonly Student _student3 = new(Id3, Name3, School3);
+    
     [Fact]
     public void Student_GetId_ReturnGuid()
     {
-        //Arrange
-        var id1 = Guid.NewGuid();
-        var id2 = Guid.NewGuid();
-        var id3 = Guid.NewGuid();
-        
-        const string name1 = "Name1";
-        const string name2 = "Name2";
-        const string name3 = "Name3";
-        
-        const string school1 = "School1";
-        const string school2 = "School2";
-        const string school3 = "School3";
-        
-        var student1 = new Student(id1, name1, school1);
-        var student2 = new Student(id2, name2, school2);
-        var student3 = new Student(id3, name3, school3);
-        
         //Act
-        var expectedId1 = student1.Id;
-        var expectedId2 = student2.Id;
-        var expectedId3 = student3.Id;
+        var expectedId1 = _student1.Id;
+        var expectedId2 = _student2.Id;
+        var expectedId3 = _student3.Id;
         
         //Assert
-        Assert.Equal(id1, expectedId1);
-        Assert.Equal(id2, expectedId2);
-        Assert.Equal(id3, expectedId3);
+        Assert.Equal(Id1, expectedId1);
+        Assert.Equal(Id2, expectedId2);
+        Assert.Equal(Id3, expectedId3);
     }
     
     [Fact]
     public void Student_GetName_ReturnString()
     {
-        //Arrange
-        var id1 = Guid.NewGuid();
-        var id2 = Guid.NewGuid();
-        var id3 = Guid.NewGuid();
-        
-        const string name1 = "Name1";
-        const string name2 = "Name2";
-        const string name3 = "Name3";
-        
-        const string school1 = "School1";
-        const string school2 = "School2";
-        const string school3 = "School3";
-        
-        var student1 = new Student(id1, name1, school1);
-        var student2 = new Student(id2, name2, school2);
-        var student3 = new Student(id3, name3, school3);
-        
         //Act
-        var expectedName1 = student1.Name;
-        var expectedName2 = student2.Name;
-        var expectedName3 = student3.Name;
+        var expectedName1 = _student1.Name;
+        var expectedName2 = _student2.Name;
+        var expectedName3 = _student3.Name;
         
         //Assert
-        Assert.Equal(name1, expectedName1);
-        Assert.Equal(name2, expectedName2);
-        Assert.Equal(name3, expectedName3);
+        Assert.Equal(Name1, expectedName1);
+        Assert.Equal(Name2, expectedName2);
+        Assert.Equal(Name3, expectedName3);
     }
 
     [Fact]
     public void Student_GetSchool_ReturnString()
     {
-        //Arrange
-        var id1 = Guid.NewGuid();
-        var id2 = Guid.NewGuid();
-        var id3 = Guid.NewGuid();
-        
-        const string name1 = "Name1";
-        const string name2 = "Name2";
-        const string name3 = "Name3";
-        
-        const string school1 = "School1";
-        const string school2 = "School2";
-        const string school3 = "School3";
-        
-        var student1 = new Student(id1, name1, school1);
-        var student2 = new Student(id2, name2, school2);
-        var student3 = new Student(id3, name3, school3);
-        
         //Act
-        var expectedSchool1 = student1.School;
-        var expectedSchool2 = student2.School;
-        var expectedSchool3 = student3.School;
+        var expectedSchool1 = _student1.School;
+        var expectedSchool2 = _student2.School;
+        var expectedSchool3 = _student3.School;
         
         //Assert
-        Assert.Equal(school1, expectedSchool1);
-        Assert.Equal(school2, expectedSchool2);
-        Assert.Equal(school3, expectedSchool3);
+        Assert.Equal(School1, expectedSchool1);
+        Assert.Equal(School2, expectedSchool2);
+        Assert.Equal(School3, expectedSchool3);
     }
 }
