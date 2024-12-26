0aa7484f54e88b65dd377a1103eb774f2450f2b3
Stefy1209 <stefangramada09@gmail.com>
2024-12-26 12:41:49 +0200

    ActivePlayer.cs added

---
+++ Jr. NBA League Romania/Domain/ActivePlayer.cs
@@ -0,0 +1,9 @@
+﻿namespace Jr._NBA_League_Romania.Domain;
+
+public class ActivePlayer(Guid id, Guid playerId, Guid gameId, byte nrPointsScored, ActivePlayerType activePlayerType) : Entity<Guid>(id)
+{
+    public Guid PlayerId { get; } = playerId;
+    public Guid GameId { get; } = gameId;
+    public byte NrPointsScored { get; } = nrPointsScored;
+    public ActivePlayerType ActivePlayerType { get; } = activePlayerType;
+}
