dd7a6402a9980a050aede53a6f6adf786ef2f48d
Stefy1209 <stefangramada09@gmail.com>
2024-12-28 17:44:00 +0200

    Equals and GetHasCode methods overrode

--- Jr. NBA League Romania/Domain/Team.cs
+++ Jr. NBA League Romania/Domain/Team.cs
@@ -3,4 +3,16 @@
 public class Team(Guid id, string name) : Entity<Guid>(id)
 {
     public string Name { get; } = name;
+
+    public override bool Equals(object? obj)
+    {
+        var team = obj as Team;
+        
+        return team != null && Id == team.Id && Name == team.Name;
+    }
+    
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Id);
+    }
 }
