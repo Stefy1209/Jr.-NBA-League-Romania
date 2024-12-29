d361a55275efb04f7488117ab1820048ffab2c2c
Stefy1209 <stefangramada09@gmail.com>
2024-12-29 21:02:29 +0200

    Equals and GetHasCode methods overrode

--- Jr. NBA League Romania/Domain/Game.cs
+++ Jr. NBA League Romania/Domain/Game.cs
@@ -5,4 +5,20 @@ public class Game(Guid id, Guid idTeam1, Guid idTeam2, DateTime dateTime) : Enti
     public Guid IdTeam1 { get; } = idTeam1;
     public Guid IdTeam2 { get; } = idTeam2;
     public DateTime DateTime { get; } = dateTime;
+
+    public override bool Equals(object? obj)
+    {
+        var game = obj as Game;
+        
+        return game != null 
+               && Id == game.Id 
+               && IdTeam1 == game.IdTeam1 
+               && IdTeam2 == game.IdTeam2 
+               && Math.Abs(DateTime.Subtract(game.DateTime).Microseconds) < 1;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, IdTeam1, IdTeam2, DateTime);
+    }
 }
